103e3747d6566686a2e8c47772bdeae170edea13
Hasan Cena <144031531+Lazboy61@users.noreply.github.com>
2025-06-03 13:25:50 +0200

    Final commit before submission

--- Album.Api/obj/Debug/net8.0/Album.Api.AssemblyInfo.cs
+++ Album.Api/obj/Debug/net8.0/Album.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Album.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d484af6adefc204670c280ac289907a6ec78ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c35c3984be3c057b084eab05f1a2b4ef18f975")]
 [assembly: System.Reflection.AssemblyProductAttribute("Album.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Album.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
